a5e56bf4a2fb924ce6ebaab37ccd71752c6f52d6
Lizandra Malta <lizlrmalta@gmail.com>
2025-11-10 14:59:46 -0300

    Initial Commit

---
+++ CleanArchitetureStrategyComposite/CleanArchitetureStrategyCompositeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitetureStrategyCompositeApp.swift
+//  CleanArchitetureStrategyComposite
+//
+//  Created by Lizandra Malta on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitetureStrategyCompositeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
